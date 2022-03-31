ff3877096527ddde99c563df2ebcfbcbc7f821e6
Stephen Dios <stephendiosdeveloper@gmail.com>
2022-03-30 23:03:56 -0400

    Refactored with operate function

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ function divide(a, b) {
     }
 }
 
+
+function operate(op, a, b) {
+    if(op == "+") {
+        return add(a,b);
+    }
+    else if(op == "-") {
+        return subtract(a,b);
+    }
+    else if(op == "*") {
+        return multiply(a,b);
+    }
+    else if(op == "/") {
+        return divide(a,b);
+    }
+}
+
 testOperators();
 
 
@@ -32,82 +48,82 @@ function testOperators() {
     console.log("Beginning test");
 ////////////////////////////////////////////////////////////////////
     console.log("Add operator test");
-    if(add(0,0) != 0) {
+    if(operate("+",0,0) != 0) {
         console.log("add(0,0) failed")
     }
 
-    if(add(1,0) != 1) {
+    if(operate("+",1,0) != 1) {
         console.log("add(1,0) failed");
     }
 
-    if(add(100, 7) != 107) {
+    if(operate("+",100, 7) != 107) {
         console.log("add(100,7) failed");
     }
 ////////////////////////////////////////////////////////////////////
     console.log("Subtract operator test");
-    if(subtract(0,0) != 0) {
+    if(operate("-",0,0) != 0) {
         console.log("subtract(0,0) failed")
     }
 
-    if(subtract(1,0) != 1) {
+    if(operate("-",1,0) != 1) {
         console.log("subtract(1,0) failed");
     }
 
-    if(subtract(0, 1) != -1) {
+    if(operate("-",0, 1) != -1) {
         console.log("subtract(0,1) failed");
     }
 
-    if(subtract(100, 7) != 93) {
+    if(operate("-",100, 7) != 93) {
         console.log("subtract(100,7) failed");
     }
 
-    if(subtract(100, 100) != 0) {
+    if(operate("-",100, 100) != 0) {
         console.log("subtract(100,100) failed");
     }
 
-    if(subtract(-5, 5) != -10) {
+    if(operate("-",-5, 5) != -10) {
         console.log("subtract(-5,5) failed");
     }
 
-    if(subtract(-5, -5) != 0) {
+    if(operate("-",-5, -5) != 0) {
         console.log("subtract(-5,-5) failed");
     }
 ////////////////////////////////////////////////////////////////////
     console.log("Multiply operator test");
-    if(multiply(0,0) != 0) {
+    if(operate("*",0,0) != 0) {
         console.log("multiply(0,0) failed")
     }
 
-    if(multiply(1,0) != 0) {
+    if(operate("*",1,0) != 0) {
         console.log("multiply(1,0) failed");
     }
 
-    if(multiply(1,5) != 5) {
+    if(operate("*",1,5) != 5) {
         console.log("multiply(1,5) failed");
     }
 
-    if(multiply(100, 7) != 700) {
+    if(operate("*",100, 7) != 700) {
         console.log("multiply(100,7) failed");
     }
 ////////////////////////////////////////////////////////////////////
     console.log("Divide operator test");
-    if(!isNaN(divide(0,0))) {
+    if(!isNaN(operate("/",0,0))) {
         console.log("divide(0,0) failed")
     }
 
-    if(!isNaN(divide(1,0))) {
+    if(!isNaN(operate("/",1,0))) {
         console.log("divide(1,0) failed");
     }
 
-    if(divide(0,1) != 0) {
+    if(operate("/",0,1) != 0) {
         console.log("divide(0,1) failed");
     }
 
-    if(divide(100, 10) != 10) {
+    if(operate("/",100, 10) != 10) {
         console.log("divide(100,10) failed");
     }
 
-    if(divide(57, 1) != 57) {
+    if(operate("/",57, 1) != 57) {
         console.log("divide(57,1) failed");
     }
 ////////////////////////////////////////////////////////////////////
